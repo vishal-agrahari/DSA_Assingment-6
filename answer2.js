8c8e152585d45b1849f5b4778f825ed0c0c5c1d3
Vishal Agrahari <128042149+vishal-agrahari@users.noreply.github.com>
2023-07-18 11:44:22 -0700

    Create answer2.js

---
+++ answer2.js
@@ -0,0 +1,33 @@
+function searchMatrix(matrix, target) {
+  const m = matrix.length;
+  const n = matrix[0].length;
+  let left = 0;
+  let right = m * n - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const row = Math.floor(mid / n);
+    const col = mid % n;
+    const midValue = matrix[row][col];
+
+    if (midValue === target) {
+      return true;
+    } else if (midValue < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return false;
+}
+
+// Test Example 1
+const matrix = [
+  [1, 3, 5, 7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 60]
+];
+const target = 3;
+const output = searchMatrix(matrix, target);
+console.log(output); // Output: true
